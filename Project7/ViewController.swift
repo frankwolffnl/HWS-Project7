3c78044065a71a27a782f385ce6f1970113a6a5e
frankwolffnl <frankwolff.nl@gmail.com>
2017-11-03 14:15:06 +0100

    Initial Commit

---
+++ Project7/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by Frank Wolff on 03/11/2017.
+//  Copyright © 2017 Frank Wolff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
