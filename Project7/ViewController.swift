b348fcb4588b66dad4224c6e49d417ca8fe11bcf
frankwolffnl <frankwolff.nl@gmail.com>
2017-11-03 14:29:01 +0100

    Add JSON library for Swift to the project.

--- Project7/ViewController.swift
+++ Project7/ViewController.swift
@@ -9,15 +9,25 @@
 import UIKit
 
 class ViewController: UITableViewController {
+	var petitions = [[String: String]]()
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+	}
+	
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return petitions.count
+	}
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+		cell.textLabel?.text = "Title goes here"
+		cell.detailTextLabel?.text = "Subtitle goes here"
+		return cell
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
 	}
 
 
